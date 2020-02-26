9678ac9c672f998e2f88f5b5810ad8c621be5325
Victor Yurkin <victor.yurkin@aetion.com>
2020-02-26 16:21:51 -0500

    Updates

--- cloudfront.tf
+++ cloudfront.tf
@@ -3,41 +3,14 @@ resource "aws_cloudfront_origin_access_identity" "default" {
 }
 
 resource "aws_cloudfront_distribution" "default" {
-  enabled             = var.enabled
-  is_ipv6_enabled     = var.is_ipv6_enabled
+  
+  # General
+  enabled             = true
   comment             = var.comment
   default_root_object = var.default_root_object
+  aliases             = var.aliases
+  web_acl_id          = var.web_acl_id
   price_class         = var.price_class
-
-  aliases = var.aliases
-
-  dynamic "custom_error_response" {
-    for_each = var.custom_error_response
-    content {
-      error_caching_min_ttl = lookup(custom_error_response.value, "error_caching_min_ttl", null)
-      error_code            = custom_error_response.value.error_code
-      response_code         = lookup(custom_error_response.value, "response_code", null)
-      response_page_path    = lookup(custom_error_response.value, "response_page_path", null)
-    }
-  }
-
-  dynamic "origin" {
-    for_each = var.origins
-    content {
-      domain_name = origin.domain_name
-      origin_path = origin.path
-      origin_id   = origin.id
-
-      custom_origin_config {
-        origin_ssl_protocols     = origin.ssl_protocols || var.default_origin_ssl_protocols
-        origin_protocol_policy   = origin.protocol_policy || var.default_origin_protocol_policy
-        origin_read_timeout      = origin.read_timeout || var.default_origin_read_timeout
-        origin_keepalive_timeout = origin.keepalive_timeout || var.default_origin_keepalive_timeout
-        http_port                = origin.http_port || var.default_origin_http_port
-        https_port               = origin.https_port || var.default_origin_https_port
-      }
-    }
-  }
 
   viewer_certificate {
     acm_certificate_arn            = var.acm_certificate_arn
@@ -46,49 +19,99 @@ resource "aws_cloudfront_distribution" "default" {
     cloudfront_default_certificate = var.acm_certificate_arn == "" ? true : false
   }
 
-  dynamic "default_cache_behavior" {
-    for_each = var.origins
-    content {
-      allowed_methods  = var.allowed_methods
-      cached_methods   = var.cached_methods
-      target_origin_id = origins.origin_id
-      compress         = var.compress
-
-      forwarded_values {
-        headers = var.forward_headers
-
-        query_string = var.forward_query_string
-
-        cookies {
-          forward           = var.forward_cookies
-          whitelisted_names = var.forward_cookies_whitelisted_names
-        }
-      }
-
-      viewer_protocol_policy = var.viewer_protocol_policy
-      default_ttl            = var.default_ttl
-      min_ttl                = var.min_ttl
-      max_ttl                = var.max_ttl
-    }
-  }
-
-  web_acl_id = var.web_acl_id
-
-  restrictions {
-    geo_restriction {
-      restriction_type = var.geo_restriction_type
-      locations        = var.geo_restriction_locations
-    }
-  }
-
-  tags = local.base_tags
-
   dynamic "logging_config" {
     for_each = var.logging_enabled ? ["true"] : []
     content {
       include_cookies = var.log_include_cookies
       bucket          = var.log_bucket_domain_name
       prefix          = var.log_prefix
+    }
+  }
+
+  tags = local.base_tags
+
+  # Origins
+  dynamic "origin" {
+    for_each = var.origins
+    content {
+      domain_name = origin.domain_name
+      origin_path = origin.path
+      origin_id   = origin.id
+
+      dynamic "custom_header" {
+        for_each = origin.custom_headers
+        content {
+          name  = custom_header.name
+          value = custom_header.value
+        }
+      }
+
+      custom_origin_config {
+        origin_ssl_protocols     = origin.ssl_protocols || var.origin_ssl_protocols_default
+        origin_protocol_policy   = origin.protocol_policy || var.origin_protocol_policy_default
+        origin_read_timeout      = origin.read_timeout || var.origin_read_timeout_default
+        origin_keepalive_timeout = origin.keepalive_timeout || var.origin_keepalive_timeout_default
+        http_port                = origin.http_port || var.origin_http_port_default
+        https_port               = origin.https_port || var.origin_https_port_default
+      }
+    }
+  }
+
+  # Behaviors
+
+  dynamic "default_cache_behavior" {
+    for_each = [var.behaviors.default]
+    content {
+      target_origin_id       = default_cache_behavior.origin_id
+      viewer_protocol_policy = default_cache_behavior.viewer_protocol_policy || var.cache_behavior_viewer_protocol_policy_default
+      allowed_methods        = default_cache_behavior.allowed_methods || var.cache_behavior_allowed_methods_default
+      cached_methods         = default_cache_behavior.cached_methods || var.cache_behavior_cached_methods_default
+      
+      forwarded_values {
+        query_string = default_cache_behavior.forward_query_string || var.cache_behavior_forwarded_values_query_string_default
+        cookies {
+          forward           = default_cache_behavior.forward_cookies || var.cache_behavior_forwarded_values_cookies_forward_default
+          whitelisted_names = default_cache_behavior.forward_cookies_whitelisted_names || var.cache_behavior_forwarded_values_cookies_whitelisted_names_default
+        }
+      }
+
+      default_ttl = default_cache_behavior.default_ttl || var.cache_behavior_default_ttl_default
+      min_ttl     = default_cache_behavior.min_ttl || var.cache_behavior_min_ttl_default
+      max_ttl     = default_cache_behavior.max_ttl || var.cache_behavior_max_ttl_default
+    }
+  }
+
+  dynamic "ordered_cache_behavior" {
+    for_each = var.behaviors.ordered
+    content {
+      path_pattern           = ordered_cache_behavior.path_pattern
+      target_origin_id       = default_cache_behavior.origin_id
+      viewer_protocol_policy = default_cache_behavior.viewer_protocol_policy || var.cache_behavior_viewer_protocol_policy_default
+      allowed_methods        = default_cache_behavior.allowed_methods || var.cache_behavior_allowed_methods_default
+      cached_methods         = default_cache_behavior.cached_methods || var.cache_behavior_cached_methods_default
+      
+      forwarded_values {
+        query_string = default_cache_behavior.forward_query_string || var.cache_behavior_forwarded_values_query_string_default
+        cookies {
+          forward           = default_cache_behavior.forward_cookies || var.cache_behavior_forwarded_values_cookies_forward_default
+          whitelisted_names = default_cache_behavior.forward_cookies_whitelisted_names || var.cache_behavior_forwarded_values_cookies_whitelisted_names_default
+        }
+      }
+
+      default_ttl = default_cache_behavior.default_ttl || var.cache_behavior_default_ttl_default
+      min_ttl     = default_cache_behavior.min_ttl || var.cache_behavior_min_ttl_default
+      max_ttl     = default_cache_behavior.max_ttl || var.cache_behavior_max_ttl_default      
+    }
+  }  
+
+  # Error Pages
+  dynamic "custom_error_response" {
+    for_each = var.custom_error_responses
+    content {
+      error_caching_min_ttl = lookup(custom_error_response.value, "error_caching_min_ttl", null)
+      error_code            = custom_error_response.value.error_code
+      response_code         = lookup(custom_error_response.value, "response_code", null)
+      response_page_path    = lookup(custom_error_response.value, "response_page_path", null)
     }
   }  
 }
--- variables.tf
+++ variables.tf
@@ -39,202 +39,195 @@ variable propagate_at_launch {
 # General
 ########################################################
 
-variable "enabled" {
-  default     = "true"
-  description = "Set to false to prevent the module from creating any resources"
+variable "comment" {
+  type        = string
+  default     = "Managed by Terraform"
+  description = "(Optional) - Comment for the origin access identity"
 }
 
-variable "acm_certificate_arn" {
-  description = "Existing ACM Certificate ARN"
-  default     = ""
+variable "default_root_object" {
+  type        = string
+  default     = "index.html"
+  description = "(Optional) - Object that CloudFront return when requests the root URL"
 }
 
 variable "aliases" {
   type        = list
   default     = []
-  description = "List of aliases. CAUTION! Names MUSTN'T contain trailing `.`"
-}
-
-variable "custom_error_response" {
-  # http://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/custom-error-pages.html#custom-error-pages-procedure
-  # https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#custom-error-response-arguments
-  description = "(Optional) - List of one or more custom error response element maps"
-  type    = list
-  default = []
+  description = "(Optional) - List of aliases. CAUTION! Names MUSTN'T contain trailing `.`"
 }
 
 variable "web_acl_id" {
+  type        = string
   description = "(Optional) - Web ACL ID that can be attached to the Cloudfront distribution"
   default     = ""
 }
 
+variable "price_class" {
+  type = string
+  description = "(Optional) - The price class for this distribution. One of PriceClass_All, PriceClass_200, PriceClass_100"
+  default = "PriceClass_All"
+}
+
 ########################################################
-# Origin object
+# Viewer Certificate
+########################################################
+
+variable "acm_certificate_arn" {
+  type        = string
+  description = "(Optional) - Existing ACM Certificate ARN"
+  default     = ""
+}
+
+variable "viewer_minimum_protocol_version" {
+  type        = string
+  description = "(Optional) - The minimum version of the SSL protocol that you want CloudFront to use for HTTPS connections."
+  default     = "TLSv1.1_2016"
+}
+
+########################################################
+# Logging Config
+########################################################
+
+variable "logging_enabled" {
+  type        = string
+  default     = "true"
+  description = "(Optional) - Wether logging config enabled"
+}
+
+variable "log_include_cookies" {
+  type        = string
+  default     = "true"
+  description = "(Optional) - Include cookies in access logs"
+}
+
+variable "log_bucket_domain_name" {
+  type        = string
+  default     = ""
+  description = "(Optional) - S3 bucket domain name for logs"
+}
+
+variable "log_prefix" {
+  type        = string
+  default     = "logs"
+  description = "(Optional) - Path of logs in S3 bucket"
+}
+
+########################################################
+# Origins
 ########################################################
 
 variable "origins" {
   type = list
-  description = "List of origin objects"
+  description = "(Required) - List of origin objects"
 }
 
-variable "default_origin_ssl_protocols" {
+variable "origin_ssl_protocols_default" {
   type        = list
-  description = "The SSL/TLS protocols that you want CloudFront to use when communicating with your origin over HTTPS"  
+  description = "(Optional) - The SSL/TLS protocols that you want CloudFront to use when communicating with your origin over HTTPS"  
   default     = ["TLSv1", "TLSv1.1", "TLSv1.2"]
 }
 
-variable "default_origin_protocol_policy" {
+variable "origin_protocol_policy_default" {
   type        = string
-  description = "The origin protocol policy to apply to your origin. One of http-only, https-only, or match-viewer"
+  description = "(Optional) - The origin protocol policy to apply to your origin. One of http-only, https-only, or match-viewer"
   default     = "match-viewer"
 }
 
-variable "default_origin_read_timeout" {
+variable "origin_read_timeout_default" {
   type        = number
-  description = "The Custom Read timeout, in seconds. By default, AWS enforces a limit of 60. But you can request an increase."
+  description = "(Optional) - The Custom Read timeout, in seconds. By default, AWS enforces a limit of 60. But you can request an increase."
   default     = 60
 }
 
-variable "default_origin_keepalive_timeout" {
+variable "origin_keepalive_timeout_default" {
   type        = number
-  description = "The Custom KeepAlive timeout, in seconds. By default, AWS enforces a limit of 60. But you can request an increase."
+  description = "(Optional) - The Custom KeepAlive timeout, in seconds. By default, AWS enforces a limit of 60. But you can request an increase."
   default     = 60
 }
 
-variable "default_origin_http_port" {
+variable "origin_http_port_default" {
   type        = number
-  description = "The HTTP port the custom origin listens on"
+  description = "(Optional) - The HTTP port the custom origin listens on"
   default     = 80
 }
 
-variable "default_origin_https_port" {
+variable "origin_https_port_default" {
   type        = number
-  description = "The HTTPS port the custom origin listens on"
+  description = "(Optional) - The HTTPS port the custom origin listens on"
   default     = 443
 }
 
+########################################################
+# Behaviors
+########################################################
 
 
-
-variable "compress" {
-  description = "(Optional) Whether you want CloudFront to automatically compress content for web requests that include Accept-Encoding: gzip in the request header (default: false)"
-  default     = "false"
-}
-
-variable "is_ipv6_enabled" {
-  default     = "true"
-  description = "State of CloudFront IPv6"
-}
-
-variable "default_root_object" {
-  default     = "index.html"
-  description = "Object that CloudFront return when requests the root URL"
-}
-
-variable "comment" {
-  default     = "Managed by Terraform"
-  description = "Comment for the origin access identity"
-}
-
-variable "logging_enabled" {
-  default     = "false"
-  description = "Wether logging config enabled"
-}
-
-variable "log_include_cookies" {
-  default     = "false"
-  description = "Include cookies in access logs"
-}
-
-variable "log_bucket_domain_name" {
-  default     = ""
-  description = "S3 bucket domain name for logs"
-}
-
-variable "log_prefix" {
-  default     = ""
-  description = "Path of logs in S3 bucket"
-}
-
-variable "forward_query_string" {
-  default     = "false"
-  description = "Forward query strings to the origin that is associated with this cache behavior"
-}
-
-variable "forward_headers" {
-  description = "Specifies the Headers, if any, that you want CloudFront to vary upon for this cache behavior. Specify `*` to include all headers."
-  type        = list
-  default     = []
-}
-
-variable "forward_cookies" {
-  description = "Specifies whether you want CloudFront to forward cookies to the origin. Valid options are all, none or whitelist"
-  default     = "none"
-}
-
-variable "forward_cookies_whitelisted_names" {
-  type        = list
-  description = "List of forwarded cookie names"
-  default     = []
-}
-
-variable "price_class" {
-  default     = "PriceClass_100"
-  description = "Price class for this distribution: `PriceClass_All`, `PriceClass_200`, `PriceClass_100`"
-}
-
-variable "viewer_minimum_protocol_version" {
-  description = "(Optional) The minimum version of the SSL protocol that you want CloudFront to use for HTTPS connections."
-  default     = "TLSv1.1_2016"
-}
-
-variable "viewer_protocol_policy" {
-  description = "allow-all, redirect-to-https"
+variable "cache_behavior_viewer_protocol_policy_default" {
+  type        = string
+  description = "(Optional) - allow-all, redirect-to-https"
   default     = "redirect-to-https"
 }
 
-variable "allowed_methods" {
+variable "cache_behavior_allowed_methods_default" {
   type        = list
-  default     = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-  description = "List of allowed methods (e.g. ` GET, PUT, POST, DELETE, HEAD`) for AWS CloudFront"
+  description = "(Optional) - List of allowed methods (e.g. ` GET, PUT, POST, DELETE, HEAD`) for AWS CloudFront"
+  default     = ["GET", "HEAD", "OPTIONS"]
 }
 
-variable "cached_methods" {
+variable "cache_behavior_cached_methods_default" {
   type        = list
-  default     = ["GET", "HEAD"]
-  description = "List of cached methods (e.g. ` GET, PUT, POST, DELETE, HEAD`)"
+  description = "(Optional) - List of cached methods (e.g. ` GET, PUT, POST, DELETE, HEAD`)"
+  default     = ["GET", "HEAD", "OPTIONS"]
 }
 
-variable "default_ttl" {
-  default     = "60"
-  description = "Default amount of time (in seconds) that an object is in a CloudFront cache"
+variable "cache_behavior_forwarded_values_query_string_default" {
+  type        = string
+  description = "(Optional) - Forward query strings to the origin that is associated with this cache behavior"
+  default     = "false"
 }
 
-variable "min_ttl" {
-  default     = "0"
-  description = "Minimum amount of time that you want objects to stay in CloudFront caches"
-}
-
-variable "max_ttl" {
-  default     = "31536000"
-  description = "Maximum amount of time (in seconds) that an object is in a CloudFront cache"
-}
-
-variable "geo_restriction_type" {
-  # e.g. "whitelist"
+variable "cache_behavior_forwarded_values_cookies_forward_default" {
+  type        = string
+  description = "(Optional) - Specifies whether you want CloudFront to forward cookies to the origin. Valid options are all, none or whitelist"
   default     = "none"
-  description = "Method that use to restrict distribution of your content by country: `none`, `whitelist`, or `blacklist`"
 }
 
-variable "geo_restriction_locations" {
-  type = list
-  # e.g. ["US", "CA", "GB", "DE"]
-  default     = []
-  description = "List of country codes for which  CloudFront either to distribute content (whitelist) or not distribute your content (blacklist)"
-}
-
-variable "cache_behavior" {
+variable "cache_behavior_forwarded_values_cookies_whitelisted_names_default" {
   type        = list
-  description = "An ordered list of cache behaviors resource for this distribution. List from top to bottom in order of precedence. The topmost cache behavior will have precedence 0."
+  description = "(Optional) - List of forwarded cookie names"
   default     = []
+}
+
+variable "cache_behavior_default_ttl_default" {
+  type        = number
+  description = "(Optional) - Default amount of time (in seconds) that an object is in a CloudFront cache"
+  default     = 86400
+}
+
+variable "cache_behavior_min_ttl_default" {
+  type        = number
+  description = "(Optional) - Minimum amount of time that you want objects to stay in CloudFront caches"
+  default     = 0
+}
+
+variable "cache_behavior_max_ttl_default" {
+  type        = number
+  description = "(Optional) - Maximum amount of time (in seconds) that an object is in a CloudFront cache"
+  default     = 31536000
+}
+
+########################################################
+# Custom Error
+########################################################
+
+variable "custom_error_responses" {
+  type        = list
+  description = "(Optional) - List of one or more custom error response element maps"
+  default     = [{
+    error_caching_min_ttl = 0
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+  }]
 }
